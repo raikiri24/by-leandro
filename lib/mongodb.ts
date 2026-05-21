752c07470b8ab051982d4e82f155384ed0032d24
Paul Lanot <paulleandrolanot@gmail.com>
2026-05-21 13:15:44 +0800

    deploy

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,17 +1,15 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("Missing MONGODB_URI environment variable.");
-}
-
-const mongoUri = uri;
 let cachedClient: MongoClient | null = null;
 let clientPromise: Promise<MongoClient> | null = null;
 
 export function getMongoClient() {
   if (cachedClient) return Promise.resolve(cachedClient);
+
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    throw new Error("Missing MONGODB_URI environment variable.");
+  }
 
   if (!clientPromise) {
     clientPromise = new MongoClient(mongoUri).connect().then((client) => {
